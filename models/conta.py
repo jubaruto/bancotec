bd8fa5ee6200ad44407952af43ea031769eec4af
Juliana Souto <barutotec@gmail.com>
2022-11-03 20:18:53 -0300

    Foram realizados reparos no código do arquivo conta

--- models/conta.py
+++ models/conta.py
@@ -2,7 +2,7 @@ from models.cliente import Cliente
 from utils.helper import formata_float_str_moeda
 
 class Conta:
-    codigo: int = 1000
+    codigo: int = 1001
     
     def __init__(self: object, cliente: Cliente) -> None:
         self.__numero: int = Conta.codigo
@@ -12,82 +12,82 @@ class Conta:
         self.__saldo_total: float = self._calcula_saldo_total
         Conta.codigo += 1
         
-def __str__(self: object) -> str:
-    return f'Número da Conta: {self.numero} \nCliente: {self.cliente.nome} ' \
-           f'\nSaldo Total: {formata_float_str_moeda(self.saldo_total)}'
+    def __str__(self: object) -> str:
+        return f'Número da Conta: {self.numero} \nCliente: {self.cliente.nome} ' \
+               f'\nSaldo Total: {formata_float_str_moeda(self.saldo_total)}'
 
-@property
-def numero(self: object) -> int:
-    return self.__numero
+    @property
+    def numero(self: object) -> int:
+        return self.__numero
 
-@property
-def cliente(self: object) -> Cliente:
-    return self.__cliente
+    @property
+    def cliente(self: object) -> Cliente:
+        return self.__cliente
 
-@property
-def saldo(self: object) -> float:
-    return self.__saldo
+    @property
+    def saldo(self: object) -> float:
+        return self.__saldo
 
-@saldo.setter
-def saldo(self: object, valor: float) -> None:
-    self.__saldo = valor
+    @saldo.setter
+    def saldo(self: object, valor: float) -> None:
+        self.__saldo = valor
 
-@property
-def limite(self: object) -> float:
-    return self.__limite
+    @property
+    def limite(self: object) -> float:
+        return self.__limite
 
-@limite.setter
-def limite(self: object, valor: float) -> None:
-    self.__limite = valor
+    @limite.setter
+    def limite(self: object, valor: float) -> None:
+        self.__limite = valor
     
-@property
-def saldo_total(self: object) -> float:
-    return self.__saldo_total
-@saldo_total.setter
-def saldo_total(self: object, valor: float) -> None:
-    self.__saldo_total = valor
+    @property
+    def saldo_total(self: object) -> float:
+        return self.__saldo_total
+    @saldo_total.setter
+    def saldo_total(self: object, valor: float) -> None:
+        self.__saldo_total = valor
 
-@property
-def _calcula_saldo_total(self: object) -> float:
-    return self.saldo + self.limite
+    @property
+    def _calcula_saldo_total(self: object) -> float:
+        return self.saldo + self.limite
 
-def depositar(self: object, valor: float) -> None:
-    if valor> 0:
-        self.saldo = self.saldo + valor
-        self.saldo_total = self._calcula_saldo_total
-        print('Depósito efetuado com sucesso!')
-    else:
-        print('Erro! Tente novamente.')
-
-def sacar(self: object, valor: float) -> None:
-    if valor > 0 and self.saldo_total >= valor:
-        if self.saldo >= valor:
-            self.saldo = self.saldo - valor
+    def depositar(self: object, valor: float) -> None:
+        if valor> 0:
+            self.saldo = self.saldo + valor
             self.saldo_total = self._calcula_saldo_total
+            print('Depósito efetuado com sucesso!')
         else:
-            restante: float = self.saldo - valor
-            self.limite = self.limite + restante
-            self.saldo = 0
-            self.saldo_total = self._calcula_saldo_total
+            print('Erro! Tente novamente.')
+
+    def sacar(self: object, valor: float) -> None:
+        if valor > 0 and self.saldo_total >= valor:
+            if self.saldo >= valor:
+                self.saldo = self.saldo - valor
+                self.saldo_total = self._calcula_saldo_total
+            else:
+                restante: float = self.saldo - valor
+                self.limite = self.limite + restante
+                self.saldo = 0
+                self.saldo_total = self._calcula_saldo_total
             print('Saque efetuado com sucesso.')
-    else:
-        print('Saque não efetuado.')
-def transferir(self: object, destino: object, valor: float) -> None:
-    if valor > 0 and self.saldo_total >= valor:
-        if self.saldo >= valor:
-            self.saldo = self.saldo - valor
-            self.saldo_total = self._calcula_saldo_total
-            destino.saldo = destino.saldo + valor
-            destino.saldo_total = destino._calcula_saldo_total
         else:
-            restante: float = self.saldo - valor
-            self.saldo = 0
-            self.limite = self.limite + restante
-            self.saldo_total = self._calcula_saldo_total
-            destino.saldo = destino.saldo + valor
-            destino.saldo_total = destino._calcula_saldo_total
-        print('Transferência efetuada.')
-    else:
-        print('Transferência não efetuada.')
+            print('Saque não efetuado.')
+    def transferir(self: object, destino: object, valor: float) -> None:
+        if valor > 0 and self.saldo_total >= valor:
+            if self.saldo >= valor:
+                self.saldo = self.saldo - valor
+                self.saldo_total = self._calcula_saldo_total
+                destino.saldo = destino.saldo + valor
+                destino.saldo_total = destino._calcula_saldo_total
+            else:
+                restante: float = self.saldo - valor
+                self.saldo = 0
+                self.limite = self.limite + restante
+                self.saldo_total = self._calcula_saldo_total
+                destino.saldo = destino.saldo + valor
+                destino.saldo_total = destino._calcula_saldo_total
+            print('Transferência efetuada.')
+        else:
+            print('Transferência não efetuada.')
 
 
